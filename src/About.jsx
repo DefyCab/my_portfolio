f345474037fe356bc31afaf1f472567c846dc0cb
DefyCab <love.bellinder@gmail.com>
2021-11-16 21:31:54 +0100

    add About.jsx and Project.jsx. Franwwork comnplete and second test pass

---
+++ src/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+const About = () => {
+  return (
+    <Container>
+      <h1 id="about-header">About Me:</h1>
+      <p>
+        I am currently trying to navigate through the mists of coding together
+        with my brave cohort of keyboard warrios.
+      </p>
+    </Container>
+  )
+}
+
+export default About
---
+++ src/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+const Projects = () => {
+  return (
+    <Container>
+      <h1 id="projects-header">My Projects</h1>
+      <p>Mumbo jumbo</p>
+    </Container>
+  )
+}
+
+export default Projects
