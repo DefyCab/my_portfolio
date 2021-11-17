10d2b7484be4fb12e730f3ee930f59f9c77b2318
DefyCab <love.bellinder@gmail.com>
2021-11-18 00:05:33 +0100

    third test all green

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -5,11 +5,11 @@ const ProjectCard = ({ project}) => {
     return (
     <Card>
         <Image src={project.image} wrapped ui={false} />
-        <Card.content>
+        <Card.Content>
             <Card.Header>{project.name}</Card.Header>
             <Card.Description>{project.description}</Card.Description>
 
-        </Card.content>
+        </Card.Content>
     </Card>
     )
 }
