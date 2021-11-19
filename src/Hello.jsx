87c6acaabc99eba7c7afb61a62e515de72abb0fb
DefyCab <love.bellinder@gmail.com>
2021-11-19 01:12:31 +0100

    Trial and error with Semantic UI

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
 
-const Hello = () => {
-  return (
-    <Container>
-      <h1 id="hello">Hello World</h1>
-    </Container>
-  )
-}
+
+
+ const Hello = () => {
+   return (
+     <Container textAlign="fluid" Image src="">
+       <h1>Welcome to my projects tab!</h1>
+     </Container>
+   )
+ }
 
 export default Hello
