35d48a8e750e71d663854de72691574c225d1788
DefyCab <love.bellinder@gmail.com>
2021-11-19 01:50:52 +0100

    format code and tidy up some

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Container } from 'semantic-ui-react'
 
-
-
- const Hello = () => {
-   return (
-     <Container textAlign="fluid" Image src="">
-       <h1>Welcome to my projects tab!</h1>
-     </Container>
-   )
- }
+const Hello = () => {
+  return (
+    <Container textAlign="fluid" Image src="">
+      <h1>Welcome to my projects tab!</h1>
+    </Container>
+  )
+}
 
 export default Hello
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,36 +1,35 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Container, Grid } from 'semantic-ui-react'
-import ProjectCard from "./ProjectCard"
-import axios from "axios"
+import ProjectCard from './ProjectCard'
+import axios from 'axios'
 
 class Projects extends Component {
   state = {
     projects: [],
   }
-    
-    render() {
-    const { projects } = this.state;
+
+  render() {
+    const { projects } = this.state
 
     let projectsList = projects.map((project) => {
       return (
         <div id={`project-${project.id}`} key={project.id}>
-          <ProjectCard project={project}/>
+          <ProjectCard project={project} />
         </div>
-      );
-    });
-
+      )
+    })
 
     return (
       <Container>
         <h1 id="projects-header">My Projects</h1>
-       <Grid>{projectsList}</Grid>
-      </Container >
-    );
+        <Grid>{projectsList}</Grid>
+      </Container>
+    )
   }
   componentDidMount() {
-    axios.get("./data/projects.json").then((response) => {
-      this.setState({ projects: response.data });
-    });
-  }  
-}      
+    axios.get('./data/projects.json').then((response) => {
+      this.setState({ projects: response.data })
+    })
+  }
+}
 export default Projects
