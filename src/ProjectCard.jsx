2a96d445d165d1d312f9e2c4b228c2fa93df0540
DefyCab <love.bellinder@gmail.com>
2021-11-21 20:28:37 +0100

    start styling with semantic ui

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -4,11 +4,16 @@ import { Card, Image } from 'semantic-ui-react'
 const ProjectCard = ({ project }) => {
   return (
     <Card>
-      <Image src={project.image} wrapped ui={false} />
-      <Card.Content>
+      <Image 
+      src={project.image} height={300}/>
+        <Card.Content>
         <Card.Header>{project.name}</Card.Header>
-        <Card.Description>{project.description}</Card.Description>
-      </Card.Content>
+        <Card.Meta> <span className="date">Completed in 2021</span> </Card.Meta>
+        <Card.Description style ={{
+          color: "blue"}}>
+          
+          {project.description}</Card.Description>
+        </Card.Content>
     </Card>
   )
 }
