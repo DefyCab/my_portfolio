c0d248ee2fecd6e473a005852b75a7f707c5decd
DefyCab <love.bellinder@gmail.com>
2021-11-21 13:20:50 +0100

    start implent cv test but test not run no suite

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -1,13 +1,64 @@
-import React from 'react'
-import { Container } from "semantic-ui-react"
+import React, { Component } from 'react'
+import { Container, Grid } from 'semantic-ui-react'
+import CvCard from './CvCard'
+import axios from 'axios'
 
-const Cv = () => {
+class Cv extends Component {
+  state = {
+    cvs: [],
+  }
+
+  render() {
+    const { cvs } = this.state
+
+    let cvsList = cvs.map((cv) => {
+      return (
+        <div id={`cv-${cv.id}`} key={cv.id}>
+          <CvCard cv={cv} />
+        </div>
+      )
+    })
 
     return (
-        <Container textAlign="fluid">
-            <h1 id="cv-header">My Cv</h1>
-        </Container>
+      <Container>
+        <h1 id="cv-header">My Cv</h1>
+        <Grid>{cvsList}</Grid>
+      </Container>
     )
+  }
+  componentDidMount() {
+    axios.get('./data/cvhistory.json').then((response) => {
+      this.setState({ cvs: response.data })
+    })
+  }
 }
+export default Cv
 
-export default Cv;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from 'react'
+// import { Container, Grid } from "semantic-ui-react"
+// import { CvCard } from "./CvCard"
+
+// const Cv = () => {
+
+//     return (
+//         <Container textAlign="fluid">
+//             <h1 id="cv-header">My Cv</h1>
+//         </Container>
+//     )
+// }
+
+// export default Cv;
